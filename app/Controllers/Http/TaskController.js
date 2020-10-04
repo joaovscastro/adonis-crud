2b54447d2feb8c1e6d46957c4af4b2bf223d1788
João Castro <joaocastrojv@gmail.com>
2020-10-03 22:58:50 -0300

    first commit

---
+++ app/Controllers/Http/TaskController.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const Task = use('App/Models/Task')
+
+/**
+ * Resourceful controller for interacting with tasks
+ */
+class TaskController {
+  async index({ params }) {
+    const tasks = await Task.query()
+      .where('project_id', params.projects_id)
+      .with('user')
+      .fetch()
+
+    return tasks
+  }
+
+  /**
+   * Create/save a new task.
+   * POST tasks
+   *
+   */
+  async store({ params, request }) {
+    const data = request.only([
+      'user_id',
+      'title',
+      'description',
+      'due_date',
+      'file_id'
+    ])
+
+    const task = await Task.create({ ...data, project_id: params.projects_id })
+
+    return task
+  }
+
+  /**
+   * Display a single task.
+   * GET tasks/:id
+   *
+   */
+  async show({ params }) {
+    const task = await Task.findOrFail(params.id)
+
+    return task
+  }
+
+  /**
+   * Update task details.
+   * PUT or PATCH tasks/:id
+   *
+   */
+  async update({ params, request }) {
+    const task = await Task.findOrFail(params.id)
+    const data = request.only([
+      'user_id',
+      'title',
+      'description',
+      'due_data',
+      'file_id'
+    ])
+
+    task.merge(data)
+
+    await task.save()
+
+    return task
+  }
+
+  /**
+   * Delete a task with id.
+   * DELETE tasks/:id
+   *
+   */
+  async destroy({ params }) {
+    const task = await Task.findOrFail(params.id)
+
+    await task.delete()
+  }
+}
+
+module.exports = TaskController
---
+++ start/routes.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Route = use('Route')
+
+Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
+
+Route.get('/files/:id', 'FileController.show')
+
+Route.group(() => {
+  Route.post('/files', 'FileController.store')
+  Route.resource('projects', 'ProjectController').apiOnly()
+
+  Route.resource('projects.tasks', 'TaskController').apiOnly()
+}).middleware(['auth'])
